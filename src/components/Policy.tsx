f2f9bc4c7da76b362149429a6b3985d35f224b0e
Saud Khan <99093073+saudkhanbpk@users.noreply.github.com>
2024-03-12 19:48:27 +0500

    new-code

--- src/components/Policy.tsx
+++ src/components/Policy.tsx
@@ -1,78 +1,93 @@
-"use client"
-import React, { useState } from 'react';
-import Image from 'next/image';
-import Polygon from '../images/Polygon.png';
-import Polygon1 from '../images/Polygon1.png';
-import about1 from '../images/about1.png';
-import policyy from '../images/policyy.png';
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Polygon from "../images/Polygon.png";
+import Polygon1 from "../images/Polygon1.png";
+import about1 from "../images/about1.png";
+import policyy from "../images/policyy.png";
+// import image1 from "../images/Rectangle.png"
+import ScheduleConsultation from "./ScheduleConsultation";
 
 const faqItems = [
   {
-    question: 'Late exit from surgery policy',
-    answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, animi?',
+    question: "Late exit from surgery policy",
+    answer:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, animi?",
   },
   {
-    question: 'Refund/Cancellation Policy',
-    answer: 'Answer for the second question.',
+    question: "Refund/Cancellation Policy",
+    answer: "Answer for the second question.",
   },
   {
-    question: 'Recovery Policy',
-    answer: 'Answer for the third question.',
+    question: "Recovery Policy",
+    answer: "Answer for the third question.",
   },
   {
-    question: 'Timing Policy',
-    answer: 'Answer for the fourth question.',
+    question: "Timing Policy",
+    answer: "Answer for the fourth question.",
+  },
+  {
+    question: "Overnight Parking Policy",
+    answer: " Clients who have packages that include overnight Recovery Assistant care are responsible for parking fees. At the conclusion of service, the client will be invoiced.",
   },
 ];
 
 const Policy = () => {
-  const [accordionStates, setAccordionStates] = useState([false]);
+  const [accordionStates, setAccordionStates] = useState(Array.from({ length: faqItems.length }, () => false));
 
-  const handleAccordionClick = (index: any) => {
-    const newAccordionStates = [...accordionStates];
-    newAccordionStates[index] = !newAccordionStates[index];
+  const handleAccordionClick = (index:any) => {
+    const newAccordionStates = accordionStates.map((state, i) => i === index ? !state : false);
     setAccordionStates(newAccordionStates);
   };
 
   return (
     <>
-      <div className="w-full flex flex-col md:flex-row justify-between items-center md:items-start ">
-        <div className="w-full md:w-auto p-4 md:p-8 text-center md:text-left">
+      <div className="w-full flex flex-col-reverse md:flex-row justify-between items-center md:items-start h-100">
+        <div className="w-full md:w-auto p-4 md:p-8 text-center md:text-left lg:ml-32">
           <div className="flex flex-col gap-4">
-            <p className="text-2xl md:text-5xl font-bold">It’s All</p>
-            <p className="text-2xl md:text-5xl font-bold">About The </p>
-            <p className="text-2xl md:text-5xl font-bold ">Fine Print Babe</p>
+            <p className="text-4xl md:text-7xl 2xl:text-8xl font-bold font-Montserrat">It’s All</p>
+            <p className="text-4xl md:text-7xl 2xl:text-8xl font-bold font-Montserrat">About The </p>
+            <p className="text-4xl md:text-6xl 2xl:text-8xl font-bold font-Montserrat">Fine Print Babe</p>
           </div>
-          <div className='flex justify-center md:justify-start mt-4 md:mt-8'>
-            <button className='bg-[#3BAFBF] px-4 py-2 text-sm md:text-base font-bold text-white'>Book a Consultation</button>
+          <div className="flex justify-center md:justify-start mt-4 md:mt-8">
+            <button className="font-Montserrat bg-[#3BAFBF] px-4 py-2 text-sm md:text-xl font-medium text-white">
+              BOOK A CONSULTATION
+            </button>
           </div>
         </div>
 
         <div className="w-full md:w-auto mt-4 md:mt-0 md:ml-4 lg:ml-8 relative">
-          <Image src={about1} alt="Logo" className="w-full md:w-[506px] lg:w-[606px] h-[480px] lg:h-[580px] mix-blend-multiply" />
+          <Image
+            src={about1}
+            alt="Logo"
+            className="w-100 mix-blend-multiply md:w-[506px] lg:w-[606px] h-[480px] lg:h-[580px]"
+          />
         </div>
       </div>
 
-      <div className="w-full relative md:-top-24 ">
-          <div className="py-12 w-full h-[141px] bg-cover bg-[url('../images/realmiami.png')]">
-            <div className="flex justify-center items-center w-auto h-[27px]">
-              <p className="text-blue-500 text-center font-niconne text-4xl lg:text-6xl font-normal">
-              Post Cosmetic Surgery Questions
-              </p>
-            </div>
+      <div className="w-full relative md:-top-24">
+        <div className="py-12 w-full h-[141px] bg-cover bg-[url('../images/realmiami.png')]">
+          <div className="flex justify-center items-center w-auto h-[27px]">
+            <p className="text-[#3BAFBF] text-center font-niconne text-6xl lg:text-6xl font-medium"
+            style={{ WebkitTextStrokeWidth: 1, WebkitTextStrokeColor: '#FFF',  }}
+            >
+              Drip N Bodied Policy
+            </p>
           </div>
         </div>
-      <div className='mb-10 text-sm font-bold flex justify-center text-md'>
-        <p className='text-[18px]'>Read our policy</p>
+      </div>
+      <div className="mb-10 text-sm font-bold flex justify-center text-md">
+        <p className="text-center font-montserrat text-black text-2xl font-bold">
+          Read our policy
+        </p>
       </div>
 
-
       <div className="w-full">
-        <div className="w-full md:w-3/4 lg:w-1/2 mx-auto p-5">
+        <div className="w-full md:w-3/4 lg:w-2/3 mx-auto p-5">
           <div className="border-t border-t-gray-300"></div>
-          <div className="bg-white px-0 py-4 rounded-md">
+          <div className="bg-white px-0 py-4 rounded-md ">
             {faqItems.map((item, index) => (
-              <div key={index} className="mb-4">
+              <div key={index} className="mb-4 space-y-3">
                 <input
                   type="checkbox"
                   id={`item${index}`}
@@ -81,20 +96,35 @@ const Policy = () => {
                 />
                 <label
                   htmlFor={`item${index}`}
-                  className="flex items-center justify-between cursor-pointer p-2 rounded-md gap-4 md:gap-8"
+                  className="flex items-center justify-between cursor-pointer p-2 rounded-md gap-4 md:gap-8 hover:bg-pink-50"
                   onClick={() => handleAccordionClick(index)}
                 >
-                  <span className="font-bold text-xl md:text-xl leading-8 md:leading-12 tracking-normal text-left">
+                  <span className="font-bold text-xl md:text-2xl leading-8 md:leading-12 tracking-normal text-left">
                     {item.question}
                   </span>
                   {accordionStates[index] ? (
-                    <Image src={Polygon1} alt="" width={50} height={50} className="w-2 h-1 md:w-3 md:h-2" />
+                    <Image
+                      src={Polygon1}
+                      alt=""
+                      width={50}
+                      height={50}
+                      className="w-2 h-1 md:w-3 md:h-2"
+                    />
                   ) : (
-                    <Image src={Polygon} alt="" width={50} height={50} className="w-2 h-1 md:w-3 md:h-2" />
+                    <Image
+                      src={Polygon}
+                      alt=""
+                      width={50}
+                      height={50}
+                      className="w-2 h-1 md:w-3 md:h-2"
+                    />
                   )}
                 </label>
                 {accordionStates[index] && (
-                  <div id={`accordion-item-${index + 1}`} className="p-4">
+                  <div
+                    id={`accordion-item-${index + 1}`}
+                    className="p-2 text-[20px]"
+                  >
                     <p>{item.answer}</p>
                   </div>
                 )}
@@ -103,26 +133,53 @@ const Policy = () => {
             ))}
           </div>
 
+          {/* <div className="mb-10 p-5">
+            <p className="pb-2 font-bold">Overnight Parking Policy</p>
+            <p>
+              Clients who have packages that include overnight Recovery
+              Assistant care are responsible for parking fees. At the conclusion
+              of service, the client will be invoiced.
+            </p>
+          </div> */}
 
-          <div className='mb-10 p-5'>
-            <p className='pb-2 font-bold'>Overnight Parking Policy</p>
-            <p>Clients who have packages that include overnight Recovery Assistant care are responsible for parking fees. At the conclusion of service, the client will be invoiced.</p>
-          </div>
-          <div className='mb-10 text-sm font-bold flex justify-center text-md p-5'>
+        </div>
+          <div className="mt-20 bg-pink-100" 
+          // style={{ background: 'linear-gradient(180deg, #FFF 0%, rgba(255, 255, 255, 0.00) 8.33%), rgba(244, 181, 217, 0.17)' }}
+          >
+            <div className="w-2/3 mx-auto">
+          <div className="mb-5 text-2xl font-bold flex justify-center text-md p-5 mt-5">
             <p>Email Us Your Question</p>
           </div>
-          <div className='mb-10 p-5'>
-            <p className='pb-2 font-bold'>What is your question?</p>
-            <input type="text" name="" id="" className='border border-gray-300 p-1 outline-none w-full' />
+          <div className="mb-5 p-5">
+            <p className="pb-2 font-bold">What is your question?</p>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter your question"
+              className="border border-gray-300 p-4 outline-none w-full"
+            />
           </div>
-          <div className='mb-8 p-5'>
-            <p className='pb-2 font-bold mb-4'>Tell us more about your question.</p>
-            <textarea name="" id="" placeholder='Enter your the details of your question' className='border border-gray-300 w-full h-[300px] outline-none mb-5 p-2 px-2'></textarea>
+          <div className="p-5">
+            <p className="pb-2 font-bold mb-4">
+              Tell us more about your question.
+            </p>
+            <textarea
+              name=""
+              id=""
+              placeholder="Enter the details of your question"
+              className="border border-gray-300 w-full h-[300px] outline-none mb-5 p-2 px-2"
+            ></textarea>
           </div>
-        </div>
+          </div>
       </div>
+      </div>
+      <div className="mb-20">
+          <ScheduleConsultation/>
+      </div>
+
     </>
   );
-}
+};
 
 export default Policy;
--- src/components/ScheduleConsultation.tsx
+++ src/components/ScheduleConsultation.tsx
@@ -19,16 +19,20 @@ const ScheduleConsultation = () => {
                 </div>
 
             </div> */}
-            <div className="h-[450px] bg-[url('../images/Rectangle.png')]">
+            <div className="h-[450px] bg-[url('../images/Rectangle.png')] bg-cover">
   <div className='flex justify-center items-center h-full'>
-    <div className='flex flex-col justify-center items-center'>
-      <h1 className='text-white text-2xl md:text-3xl lg:text-4xl mt-2 mb-4'>Talk To Your Nurse Babe</h1>
-      <div className='border-2 border-blue-300 h-1 w-[200px] md:w-[300px] mx-auto'></div>
+    <div className='flex flex-col justify-center items-center space-y-3'>
+      <h1 className='text-white text-2xl md:text-3xl lg:text-4xl mt-2 mb-4 text-center font-montserrat font-semibold'>Talk To Your Nurse Babe</h1>
+      <div className='border-2 border-[#3BAFBF] h-1 w-[200px] md:w-[300px] mx-auto'></div>
       <hr />
-      <p className='px-2 pt-2 my-2 text-white text-sm md:text-base'>Schedule A Free Consultation!</p>
-      <p className='px-2 pt-2 text-white text-sm md:text-base'>Talk one on one with your Drip N Bodied nurse babe!</p>
-      <p className='px-2 pt-2  text-white text-sm md:text-base text-center'>We are ready and willing to answer questions related to surgery, nursing, or business start-ups.</p>
-      <button className='p-2 mt-8 md:mt-12 text-white bg-[#3BAFBF]'>SCHEDULE A CONSULT CALL</button>
+      <p className='px-2 pt-2 my-2 text-white text-sm md:text-base text-center font-montserrat font-bold leading-7 '>Schedule A Free Consultation!</p>
+      <div className='text-center leading-none'>
+      <p className='px-2 pt-2 text-white text-sm md:text-base font-montserrat font-normal'>Talk one on one with your Drip N Bodied nurse babe!</p>
+      <p className='px-2 pt-2  text-white text-sm md:text-base text-center font-montserrat font-normal'>We are ready and willing to answer questions related to surgery, nursing, or business start-ups.</p>
+      </div>
+      <div>
+      <button className='p-2 mt-8 md:mt-10 text-white bg-[#3BAFBF] font-montserrat font-bold px-10 py-3'>SCHEDULE A CONSULT CALL</button>
+    </div>
     </div>
   </div>
 </div>
