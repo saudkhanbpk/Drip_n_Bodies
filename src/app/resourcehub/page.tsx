6668e0e112eba0046e78f32d855ee0c1af62e7c9
saudkhan <saudkhanbpk@gmail.com>
2024-03-18 16:00:34 +0500

    resourse hub component created

---
+++ src/app/resourcehub/page.tsx
@@ -0,0 +1,12 @@
+import ResourceHub from '@/components/ResourceHub'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <ResourceHub/>
+    </div>
+  )
+}
+
+export default page
