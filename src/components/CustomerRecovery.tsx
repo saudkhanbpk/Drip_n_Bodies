f2c6053bdbe7241317689a0eca49e01ed455d604
Saud Khan <99093073+saudkhanbpk@users.noreply.github.com>
2024-03-26 00:09:55 +0500

    changes

--- src/components/CustomerRecovery.tsx
+++ src/components/CustomerRecovery.tsx
@@ -381,7 +381,7 @@ const CustomerRecovery: React.FC = () => {
             </div>
             <div className="border-r-[5px] border-[#F4B5D9] h-60 my-auto max-md:hidden"></div>
             <div className="md:w-1/2 text-start w-full  ">
-              <div className="bg-[#F4B5D9] p-8 w-[90%] lg:w-[60%] mx-auto mt-20">
+              <div className="bg-[#F4B5D9] p-8 w-[90%] lg:w-[60%] mx-auto mt-20 shadow-2xl">
                 <ul className="list-disc pl-6 space-y-5">
                   {points.map((point, index) => (
                     <li
